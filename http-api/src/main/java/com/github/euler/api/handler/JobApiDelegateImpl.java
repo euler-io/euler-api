0e715d7b4d6ddf6bfdce35e7ea37e43f0bab110e
Filipe Simoes <835586+filipesimoes@users.noreply.github.com>
2020-06-05 00:23:20 -0300

    API Queue

--- http-api/src/main/java/com/github/euler/api/handler/JobApiDelegateImpl.java
+++ http-api/src/main/java/com/github/euler/api/handler/JobApiDelegateImpl.java
@@ -2,6 +2,7 @@ package com.github.euler.api.handler;
 
 import java.io.IOException;
 import java.net.URI;
+import java.net.URISyntaxException;
 import java.time.OffsetDateTime;
 import java.util.UUID;
 
@@ -10,11 +11,11 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.github.euler.api.APICommand;
 import com.github.euler.api.JobToEnqueue;
 import com.github.euler.api.model.Job;
+import com.github.euler.api.model.JobConfig;
 import com.github.euler.api.model.JobDetails;
 import com.github.euler.api.model.JobStatus;
 import com.github.euler.api.persistence.JobDetailsPersistence;
@@ -42,51 +43,40 @@ public class JobApiDelegateImpl implements JobApiDelegate {
     }
 
     @Override
-    public ResponseEntity<Job> createNewJob(Object config) {
+    public ResponseEntity<Job> createNewJob(JobConfig body) {
         String id = UUID.randomUUID().toString();
         try {
-            enqueueJob(id, config);
             JobDetails jobDetails = new JobDetails();
             jobDetails.setId(id);
             jobDetails.setStartDate(OffsetDateTime.now());
             jobDetails.setStatus(JobStatus.NEW);
-            jobDetails.setConfig(config);
+            jobDetails.setConfig(body.getConfig());
+            jobDetails.setSeed(body.getSeed());
             Job job = JobUtils.fromDetails(jobDetailsPersistence.create(jobDetails));
             return new ResponseEntity<Job>(job, HttpStatus.OK);
         } catch (IOException e) {
-            stopJob(id);
-            throw new RuntimeException(e);
-        }
-    }
-
-    protected void enqueueJob(String id, Object raw) throws JsonProcessingException {
-        String json = objectMapper.writer().writeValueAsString(raw);
-        Config config = ConfigFactory.parseString(json);
-        URI uri = null;
-        JobToEnqueue msg = new JobToEnqueue(id, uri, config);
-        system.tell(msg);
-    }
-
-    protected void stopJob(String id) {
-
-    }
-
-    @Override
-    public ResponseEntity<Void> deleteJob(String jobId) {
-        try {
-            jobPersistence.delete(jobId);
-            return new ResponseEntity<Void>(HttpStatus.OK);
-        } catch (IOException e) {
             throw new RuntimeException(e);
         }
     }
 
     @Override
-    public ResponseEntity<Void> changeJobStatus(String jobId, JobStatus status) {
+    public ResponseEntity<Void> cancelJob(String jobId) {
+        // TODO Auto-generated method stub
+        return JobApiDelegate.super.cancelJob(jobId);
+    }
+
+    @Override
+    public ResponseEntity<Void> enqueueJob(String jobId) {
         try {
-            jobPersistence.updateStatus(jobId, status);
+            JobDetails jobDetails = jobDetailsPersistence.get(jobId);
+            Object rawConfig = jobDetails.getConfig();
+            String json = objectMapper.writer().writeValueAsString(rawConfig);
+            URI uri = new URI(jobDetails.getSeed());
+            Config config = ConfigFactory.parseString(json);
+            JobToEnqueue msg = new JobToEnqueue(jobId, uri, config);
+            system.tell(msg);
             return new ResponseEntity<Void>(HttpStatus.OK);
-        } catch (IOException e) {
+        } catch (IOException | URISyntaxException e) {
             throw new RuntimeException(e);
         }
     }
