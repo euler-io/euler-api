ac7c8ef48dd26050a9b75bff825a52f672004b5b
filipecsimoes <835586+filipesimoes@users.noreply.github.com>
2020-07-02 13:22:15 -0300

    elasticsearch

---
+++ elasticsearch-impl/src/main/java/com/github/euler/api/WaitElasticSearchTask.java
@@ -0,0 +1,43 @@
+package com.github.euler.api;
+
+import java.io.IOException;
+import java.util.concurrent.Callable;
+
+import org.elasticsearch.ElasticsearchException;
+import org.elasticsearch.client.RequestOptions;
+import org.elasticsearch.client.RestHighLevelClient;
+import org.elasticsearch.client.core.MainResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class WaitElasticSearchTask implements Callable<MainResponse> {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(WaitElasticSearchTask.class);
+
+    private final RestHighLevelClient client;
+    private int maxRetries = 30;
+    private long interval = 2000;
+
+    public WaitElasticSearchTask(RestHighLevelClient client) {
+        super();
+        this.client = client;
+    }
+
+    @Override
+    public MainResponse call() throws Exception {
+        int attempts = 0;
+        while (attempts < maxRetries) {
+            try {
+                MainResponse resp = client.info(RequestOptions.DEFAULT);
+                LOGGER.info("Connected to Elasticsearch.");
+                return resp;
+            } catch (IOException | ElasticsearchException e) {
+                LOGGER.info("Could not connect to Elasticsearch. Retry will occur in {}ms.", interval);
+                attempts++;
+                Thread.sleep(interval);
+            }
+        }
+        throw new IllegalStateException("Could not connect to Elasticsearch.");
+    }
+
+}
---
+++ elasticsearch-impl/src/main/java/com/github/euler/api/WaitElasticsearchBean.java
@@ -0,0 +1,38 @@
+package com.github.euler.api;
+
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
+
+import javax.annotation.PostConstruct;
+
+import org.elasticsearch.client.RestHighLevelClient;
+import org.elasticsearch.client.core.MainResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component("com.github.euler.api.WaitElasticsearchBean")
+public class WaitElasticsearchBean {
+
+    private RestHighLevelClient client;
+
+    @Autowired
+    public WaitElasticsearchBean(RestHighLevelClient client) {
+        super();
+        this.client = client;
+    }
+
+    @PostConstruct
+    public void waitForElasticSearch() throws ExecutionException {
+        WaitElasticSearchTask task = new WaitElasticSearchTask(client);
+        Future<MainResponse> future = Executors.newSingleThreadExecutor().submit(task);
+        try {
+            future.get(3, TimeUnit.MINUTES);
+        } catch (InterruptedException | TimeoutException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+}
