921e870840defc4e533c974669bf367a6e9da14a
Filipe Simoes <835586+filipesimoes@users.noreply.github.com>
2020-07-01 22:30:53 -0300

    docker-compose

---
+++ elasticsearch-impl/docker/entrypoint.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+set -e
+
+if [ ! -d "$PWD/dev-certificates" ]; then
+	generate_certificates.sh
+fi
+
+mvn clean spring-boot:run \
+	-Dmaven.repo.local=/mvn/.m2/repository \
+	-Dspring-boot.run.jvmArguments="-agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=0.0.0.0:8000 \
+	-Dconfig.file=/src/docker/dev.conf"
