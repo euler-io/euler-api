43df138be4921ebf9406e5593a4e15a8a15d6495
filipecsimoes <835586+filipesimoes@users.noreply.github.com>
2020-07-03 14:50:06 -0300

    auth

---
+++ http-api/src/main/java/com/github/euler/api/SwaggerAuthConfig.java
@@ -0,0 +1,51 @@
+package com.github.euler.api;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+
+import io.swagger.configuration.SwaggerDocumentationConfig;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.service.ApiKey;
+import springfox.documentation.service.AuthorizationScope;
+import springfox.documentation.service.SecurityReference;
+import springfox.documentation.spi.service.contexts.SecurityContext;
+import springfox.documentation.spring.web.plugins.Docket;
+
+@Configuration
+public class SwaggerAuthConfig extends SwaggerDocumentationConfig {
+
+    public static final String DEFAULT_INCLUDE_PATTERN = "/.*";
+
+    @Bean
+    @Primary
+    @Override
+    public Docket customImplementation() {
+        return super.customImplementation()
+                .securityContexts(Arrays.asList(securityContext()))
+                .securitySchemes(Arrays.asList(apiKey()));
+    }
+
+    private ApiKey apiKey() {
+        return new ApiKey("euler-auth", "JWT Authorization", "header");
+    }
+
+    private SecurityContext securityContext() {
+        return SecurityContext.builder()
+                .securityReferences(defaultAuth())
+                .forPaths(PathSelectors.regex(DEFAULT_INCLUDE_PATTERN))
+                .build();
+    }
+
+    List<SecurityReference> defaultAuth() {
+        AuthorizationScope[] authorizationScopes = new AuthorizationScope[]{
+                new AuthorizationScope("global", "accessEverything")
+        };
+        return Arrays.asList(
+                new SecurityReference("JWT", authorizationScopes));
+    }
+
+}
