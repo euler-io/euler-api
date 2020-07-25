b61c5f45d1153e7330a02333d0f0cc57e33464d7
Filipe Simoes <835586+filipesimoes@users.noreply.github.com>
2020-07-25 18:35:19 -0300

    Docker images

--- opendistro-impl/docker/download-app.sh
+++ opendistro-impl/docker/download-app.sh
@@ -5,10 +5,10 @@ set -e
 mkdir -p /app/lib
 
 if [[ ${PROJECT_VERSION} == *"SNAPSHOT"* ]]; then
-	snapshotVersion=$(curl -s https://oss.sonatype.org/content/repositories/snapshots/com/github/euler-io/elasticsearch-impl/${PROJECT_VERSION}/maven-metadata.xml | grep '<value>' | head -1 | sed "s/.*<value>\([^<]*\)<\/value>.*/\1/")
-	repo="https://oss.sonatype.org/content/repositories/snapshots/com/github/euler-io/elasticsearch-impl/${PROJECT_VERSION}/elasticsearch-impl-${snapshotVersion}.jar"
+	snapshotVersion=$(curl -s https://oss.sonatype.org/content/repositories/snapshots/com/github/euler-io/opendistro-impl/${PROJECT_VERSION}/maven-metadata.xml | grep '<value>' | head -1 | sed "s/.*<value>\([^<]*\)<\/value>.*/\1/")
+	repo="https://oss.sonatype.org/content/repositories/snapshots/com/github/euler-io/opendistro-impl/${PROJECT_VERSION}/opendistro-impl-${snapshotVersion}.jar"
 else
-	repo="https://repo1.maven.org/maven2/com/github/euler-io/elasticsearch-impl/${PROJECT_VERSION}/elasticsearch-impl-${PROJECT_VERSION}.jar"
+	repo="https://repo1.maven.org/maven2/com/github/euler-io/opendistro-impl/${PROJECT_VERSION}/opendistro-impl-${PROJECT_VERSION}.jar"
 fi
 
 curl ${repo} -o /app/opendistro-http-api.jar
