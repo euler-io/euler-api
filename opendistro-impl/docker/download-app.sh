1963c52d77ca387ebeb9e0b2bc7cb029f19586dc
Filipe Simoes <835586+filipesimoes@users.noreply.github.com>
2020-08-01 18:54:24 -0300

    Docker images

--- opendistro-impl/docker/download-app.sh
+++ opendistro-impl/docker/download-app.sh
@@ -5,7 +5,7 @@ set -e
 mkdir -p /app/lib
 
 if [[ ${PROJECT_VERSION} == *"SNAPSHOT"* ]]; then
-	snapshotVersion=$(curl -s https://oss.sonatype.org/content/repositories/snapshots/com/github/euler-io/opendistro-impl/${PROJECT_VERSION}/maven-metadata.xml | grep '<value>' | head -1 | sed "s/.*<value>\([^<]*\)<\/value>.*/\1/")
+	snapshotVersion=$(curl -s https://oss.sonatype.org/content/repositories/snapshots/com/github/euler-io/opendistro-impl/${PROJECT_VERSION}/maven-metadata.xml | grep '<value>' | sort -r | head -1 | sed "s/.*<value>\([^<]*\)<\/value>.*/\1/")
 	repo="https://oss.sonatype.org/content/repositories/snapshots/com/github/euler-io/opendistro-impl/${PROJECT_VERSION}/opendistro-impl-${snapshotVersion}.jar"
 else
 	repo="https://repo1.maven.org/maven2/com/github/euler-io/opendistro-impl/${PROJECT_VERSION}/opendistro-impl-${PROJECT_VERSION}.jar"
