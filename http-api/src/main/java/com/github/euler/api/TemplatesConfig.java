e5425535e09ea167f122106b5ab6a1c5d832ca6b
Filipe Simoes <835586+filipesimoes@users.noreply.github.com>
2020-10-17 23:45:49 -0300

    #14

--- http-api/src/main/java/com/github/euler/api/TemplatesConfig.java
+++ http-api/src/main/java/com/github/euler/api/TemplatesConfig.java
@@ -5,6 +5,8 @@ import java.net.URL;
 
 import javax.annotation.PostConstruct;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 
@@ -14,10 +16,13 @@ import com.typesafe.config.Config;
 import com.typesafe.config.ConfigFactory;
 import com.typesafe.config.ConfigList;
 import com.typesafe.config.ConfigObject;
+import com.typesafe.config.ConfigRenderOptions;
 import com.typesafe.config.ConfigValue;
 
 @Configuration
 public class TemplatesConfig {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(TemplatesConfig.class);
 
     private static final String EULER_HTTP_API_TEMPLATES_URL = "euler.http-api.templates-url";
 
@@ -35,11 +40,14 @@ public class TemplatesConfig {
     protected void loadDefaultTemplates() throws IOException {
         if (config.getConfig().hasPath(EULER_HTTP_API_TEMPLATES_URL)) {
             String templatesUrl = config.getConfig().getString(EULER_HTTP_API_TEMPLATES_URL);
+            LOGGER.info("Loading default templates from {}.", templatesUrl);
             Config templatesConfig = ConfigFactory.parseURL(new URL(templatesUrl));
             ConfigList templatesList = templatesConfig.getList("templates");
             for (ConfigValue templateConfig : templatesList) {
                 loadTemplate(((ConfigObject) templateConfig).toConfig());
             }
+        } else {
+            LOGGER.info("Default templates not found.");
         }
     }
 
@@ -47,10 +55,17 @@ public class TemplatesConfig {
         String name = config.getString("name");
         TemplateDetails details = templatePersistence.get(name);
         if (details == null) {
+            LOGGER.info("Creating index {}.", name);
+            if (LOGGER.isDebugEnabled()) {
+                LOGGER.debug("Template {}", name);
+                LOGGER.debug("{}", config.root().render(ConfigRenderOptions.defaults()));
+            }
             details = new TemplateDetails();
             details.setConfig(config.withOnlyPath("config"));
             details.setName(name);
             templatePersistence.create(details);
+        } else {
+            LOGGER.info("Template {} already exists.", name);
         }
     }
 
